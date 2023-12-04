61da7e38c4cb2cdd129c17c1cda5da02555388f7
Varinder Pal Singh <varinderpalsingh@Varinders-MacBook-Air.local>
2023-12-04 22:26:58 +0530

    Added node server for nextjs hosting

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
+import payload from "payload";
 import { InitOptions } from "payload/config";
 
 dotenv.config({
@@ -16,10 +17,31 @@ if (!cached) {
 }
 
 interface Args {
-  intiOptions?: Partial<InitOptions>;
+  initOptions?: Partial<InitOptions>;
 }
 
 export const getPayloadClient = async ({ initOptions }: Args) => {
   if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is missing");
   }
+
+  if (cached.client) {
+    return cached.client;
+  }
+  if (!cached.promise) {
+    return (cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    }));
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (error: unknown) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.client;
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 const start = async () => {
-  const payload = await getPayloadClient();
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info("Admin url", cms.getAdminURL());
+      },
+    },
+  });
+  app.use((req, res) => nextHandler(req, res));
+  nextApp.prepare().then(() => {
+    payload.logger.info("Nextjs stated");
+
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Nextj App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
+    });
+  });
 };
 
 start();
